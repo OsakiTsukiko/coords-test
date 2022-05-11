26dfb88cb6b96854e0701f562b8ea5de4c8e4c9f
OsakiTsukiko <osakitsukiko.mail@gmail.com>
2022-05-12 00:24:43 +0300

    coords

--- root.gd
+++ root.gd
@@ -6,6 +6,7 @@ var player = preload("res://player.tscn")
 var tile = preload("res://tile.tscn")
 onready var actors = $actors
 onready var tiles = $tiles
+onready var label = $CanvasLayer/Label
 
 func transform_to_global_coords ( coords: Vector2 ) -> Vector2:
 	return Vector2(floor(coords.x / STEP), floor(coords.y / STEP))
@@ -20,14 +21,14 @@ func _ready():
 	pass
 	
 func _process(delta):
-	if Input.is_action_just_pressed("ui_accept"):
-		for idx in actors.get_children():
-			print(transform_to_global_coords(idx.position))
+#	if Input.is_action_just_pressed("ui_accept"):
+	for idx in actors.get_children():
+		label.text = String(transform_to_global_coords(idx.position))
 
-func _input(event):
-	if event is InputEventMouseButton:
-		print("Mouse Click/Unclick at: ", event.position)
-		var tile_instance = tile.instance()
-		tile_instance.translate(transform_to_global_coords(event.position) * STEP)
-		tiles.add_child(tile_instance)
+#func _input(event):
+#	if event is InputEventMouseButton:
+#		print("Mouse Click/Unclick at: ", event.position)
+#		var tile_instance = tile.instance()
+#		tile_instance.translate(transform_to_global_coords(event.position) * STEP)
+#		tiles.add_child(tile_instance)
 
